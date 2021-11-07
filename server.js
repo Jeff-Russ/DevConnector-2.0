121155ef14aeea62e403523460b755513ce906a2
Jeffrey Russ <jeffreylynnruss@gmail.com>
2021-11-07 14:46:59 -0500

    Add basic server and setup package.json scripts

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+
+const app = express();
+
+// connectDB(); // Connect to Database
+
+app.get('/', (req, res) => res.send('API Running')); // test endpoint
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
