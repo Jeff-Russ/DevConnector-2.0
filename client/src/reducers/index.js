b8d4cf9b19df513cc4da064a9d5839d772356a2e
Jeffrey Russ <jeffreylynnruss@gmail.com>
2021-11-23 17:26:26 -0500

    Add user registration on client-side (Auth Reducer & Register Action)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
+import auth from './auth';
 
 // this will be imported as rootReducer:
-export default combineReducers({ alert }); 
+export default combineReducers({
+  alert,
+  auth
+}); 
