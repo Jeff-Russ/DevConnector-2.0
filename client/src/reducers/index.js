39e39a6db6b150e657c5020ca6252dcb30da3311
Jeffrey Russ <jeffreylynnruss@gmail.com>
2021-11-27 14:12:37 -0500

    (Broken) ProtectedRoute, profile reducer & action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 // this will be imported as rootReducer:
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 }); 
